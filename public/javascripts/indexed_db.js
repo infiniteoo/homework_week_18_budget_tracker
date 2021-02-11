3812d547a9d2a190c9e0a5873a82d8d6b921c923
Troy Dorman <troydorman@gmail.com>
2021-02-11 14:18:48 -0700

    everything works now

--- public/javascripts/indexed_db.js
+++ public/javascripts/indexed_db.js
@@ -2,7 +2,7 @@
 let db
 
 // make request to db to open
-const request = indexedDB.open('transactions', 1)
+const request = indexedDB.open('new_transaction', 1)
 request.onerror = console.error
 // if no errors, assign a database to the callback of the request
 
@@ -10,7 +10,7 @@ request.onerror = console.error
 request.onupgradeneeded = () => {
 // create object store & key path
   const store = db.createObjectStore(
-    'transactions',
+    'new_transaction',
     {
       keyPath: 'value',
       autoIncrement: true
@@ -21,19 +21,50 @@ request.onupgradeneeded = () => {
   store.createIndex('value')
 }
 request.onsuccess = () => {
-  const db = request.result
+  db = event.target.result
 
   // if we're online, upload any stored transactions
   if (navigator.onLine) { uploadTransaction() }
 }
 
+// eslint-disable-next-line no-unused-vars
 function saveRecord (record) {
 // create transaction object
-  const trans = db.transaction(['transactions', 'readwrite'])
-  trans.onerror = console.error
+  db.transaction(['new_transaction', 'readwrite'])
+  /*  trans.onerror = console.error */
   // get object store from the transaction object
-  const store = trans.objectStore('transactions')
+  const store = db.objectStore('new_transaction')
   // write to the database
   store.add(record)
 }
 
+function uploadTransaction () {
+  // create transaction object
+  const trans = db.transaction(['new_transaction', 'readwrite'])
+  trans.onerror = console.error
+  // get object store from the transaction object
+  const store = trans.objectStore('new_transaction')
+  // grab any records stored in the local db
+  const allItemsInStore = store.getAll()
+  allItemsInStore.onsuccess = () => {
+    fetch('/api/transaction', {
+      method: 'POST',
+      body: JSON.stringify(allItemsInStore.result)
+    })
+      .then(response => response.json())
+      .then(reply => {
+        if (reply.message) { throw new Error(reply) }
+
+        const trans = db.transaction(['new_transaction', 'readwrite'])
+        trans.onerror = console.error
+        // get object store from the transaction object
+        const store = trans.objectStore('new_transaction')
+        // clear out the object store once we've uploaded the stored data to the server
+        store.clear()
+        alert('All saved transactions has been submitted!')
+      })
+      .catch(error => { console.log(error) })
+  }
+}
+
+window.addEventListener('online', uploadTransaction)
